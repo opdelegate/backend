c44fa8054bedd074197919a6119bd42648ab05d3
Ben Riazy <hamon.riazy@gmail.com>
2023-11-22 07:24:57 +0100

    fix: set header in the error response so that we at least can see the error on the frontend as well

--- src/get_daily_data/app.py
+++ src/get_daily_data/app.py
@@ -12,22 +12,21 @@ def lambda_handler(event, context):
     s3_path = f"daily_vote_data/{current_date}/{delegate}.json"
     
     s3 = boto3.client('s3')
-    
+
+    # Dynamically set the 'Access-Control-Allow-Origin' header
+    allowed_origins = ['https://opdelegate.com']
+    origin = event['headers'].get('Origin')
+    cors_header = {'Access-Control-Allow-Origin': origin} if origin in allowed_origins else {}
+    # allow any localhost
+    if origin and origin.startswith('http://localhost'):
+        cors_header = {'Access-Control-Allow-Origin': origin}
+    # also allow any vercel domain
+    if origin and origin.endswith('.vercel.app'):
+        cors_header = {'Access-Control-Allow-Origin': origin}
+
+    print(cors_header)
+
     try:
-        
-        # Dynamically set the 'Access-Control-Allow-Origin' header
-        allowed_origins = ['https://opdelegate.com']
-        origin = event['headers'].get('Origin')
-        cors_header = {'Access-Control-Allow-Origin': origin} if origin in allowed_origins else {}
-        # allow any localhost
-        if origin and origin.startswith('http://localhost'):
-            cors_header = {'Access-Control-Allow-Origin': origin}
-        # also allow any vercel domain
-        if origin and origin.endswith('.vercel.app'):
-            cors_header = {'Access-Control-Allow-Origin': origin}
-
-        print(cors_header)
-
         # Fetch the JSON data from the specified S3 path
         response = s3.get_object(Bucket=bucket_name, Key=s3_path)
         data = response['Body'].read().decode('utf-8')
@@ -44,5 +43,8 @@ def lambda_handler(event, context):
         return {
             'statusCode': 500,
             'body': str(e),
-            'variables': s3_path
+            'variables': s3_path,
+            'headers': {
+                **cors_header,
+            }
         }
